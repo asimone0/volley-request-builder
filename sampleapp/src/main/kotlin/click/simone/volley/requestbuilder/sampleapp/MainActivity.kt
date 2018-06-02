90aaea6986eaf4c352dba2e1366276da0f5742c8
Anthony <another.simone@gmail.com>
2018-06-02 00:26:45 -0400

    Update UI

--- sampleapp/src/main/kotlin/click/simone/volley/requestbuilder/sampleapp/MainActivity.kt
+++ sampleapp/src/main/kotlin/click/simone/volley/requestbuilder/sampleapp/MainActivity.kt
@@ -7,6 +7,7 @@ import click.simone.volley.requestbuilder.JSONObjectRequestBuilder
 import com.android.volley.Response
 import com.android.volley.VolleyLog
 import com.android.volley.toolbox.Volley
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.concurrent.TimeUnit
 
 
@@ -19,8 +20,8 @@ class MainActivity : AppCompatActivity() {
         val requestQueue = Volley.newRequestQueue(this)
         val r = JSONObjectRequestBuilder()
                 .url("https://jsonplaceholder.typicode.com/posts/1")
-                .listener(Response.Listener { response -> println("JSON fetch: " + response) })
-                .errorListener(Response.ErrorListener { error -> println("JSON fetch: " + error) })
+                .listener(Response.Listener { response -> content.text = response.toString() })
+                .errorListener(Response.ErrorListener { error -> content.text = error.message })
                 .cacheExpirarion(CacheExpiration(1, TimeUnit.MINUTES))
                 .build()
         requestQueue.add(r)
